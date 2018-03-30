5afb6b246405f03d476a9e8af81346c44514634e
Fábio Santos <fabiosantosart@gmail.com>
2018-03-30 23:54:01 +0100

    automatic await

--- test/cli.js
+++ test/cli.js
@@ -2,10 +2,16 @@ const assert = require('assert')
 const cli = require('../lib/cli')
 
 describe('cli', () => {
-  it('can run code', () => {
+  it('can run code', async () => {
     assert.equal(
-      cli([ '-e', '(+ 1 1)']),
+      await cli([ '-e', '(+ 1 1)']),
       2
+    )
+  })
+  it('can compile code', () => {
+    assert.equal(
+      cli(['-c', '(+ 1 1)']),
+      '(async () => {\nreturn 1 + 1})()'
     )
   })
 })
--- test/compile.js
+++ test/compile.js
@@ -1,5 +1,5 @@
 const assert = require('assert')
-const compile = require('../lib/compile')
+const compile = (s) => require('../lib/compile')(s, false)
 
 describe('compiler', () => {
   it('compiles to JS', () => {
@@ -9,7 +9,7 @@ describe('compiler', () => {
     )
     assert.equal(
       compile('(set foo "foo")\n(console.log foo)'),
-      'var foo = "foo"\nconsole.log(foo)\n'
+      'var foo = await "foo"\nawait console.log(foo)\n'
     )
     assert.equal(
       compile('(if 1 0 1)'),
@@ -17,15 +17,23 @@ describe('compiler', () => {
     )
     assert.equal(
       compile('(fn foo (n) 1)\n(foo 1)'),
-      'function foo(n) { return 1 }\nfoo(1)\n'
+      'async function foo(n) { n = await n; return 1 }\nawait foo(1)\n'
+    )
+    assert.equal(
+      compile('(fn foo [n] 1)\n(foo 1)'),
+      'async function foo(n) { n = await n; return 1 }\nawait foo(1)\n'
+    )
+    assert.equal(
+      compile('(map (fn foo (n) 1))'),
+      'await map(async function foo(n) { n = await n; return 1 })\n'
     )
     assert.equal(
       compile('(set x [1 2 3])'),
-      'var x = [1, 2, 3]\n'
+      'var x = await [1, 2, 3]\n'
     )
     assert.equal(
       compile('(set x {"foo" "bar"})'),
-      'var x = {"foo": "bar"}\n'
+      'var x = await {"foo": "bar"}\n'
     )
   })
 })
