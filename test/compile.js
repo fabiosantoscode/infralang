a2c3206c14841d6928e4cf85490137ce9ede6ba1
Fábio Santos <fabiosantosart@gmail.com>
2018-04-18 17:37:41 +0100

    refactor

--- test/compile.js
+++ test/compile.js
@@ -9,7 +9,7 @@ describe('compiler', () => {
     )
     assert.equal(
       compile('(set foo "foo")\n(console.log foo)'),
-      'var foo = await "foo"\nawait console.log(foo)\n'
+      'var foo = "foo"\nawait console.log(foo)\n'
     )
     assert.equal(
       compile('(if 1 0 1)'),
@@ -33,11 +33,11 @@ await foo(1)
     )
     assert.equal(
       compile('(set x [1 2 3])'),
-      'var x = await [1, 2, 3]\n'
+      'var x = [1, 2, 3]\n'
     )
     assert.equal(
       compile('(set x {"foo" "bar"})'),
-      'var x = await {"foo": "bar"}\n'
+      'var x = {"foo": "bar"}\n'
     )
   })
   it('compiles dependencies in the closure', () => {
